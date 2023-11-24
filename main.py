626254494fb1a6d1982b317c6d288495d182a11d
asya299 <149185200+asya299@users.noreply.github.com>
2023-11-24 16:41:20 +0000

    veri tipleri

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 #print('Hello World')
-print("hello world")
+#print("hello world")
+#print("Phyton ile ilk günüm!")
+#print("Fonksiyon şu şekilde tanımlanır: print('ekrana yazdırılacak yazı')")
+#print("Merhaba Dünya\nMerhaba Dünya\nMerhaba Dünya\n")
+#print("Merhaba Dünya")
+#print("Merhaba Dünya")
+#print("Merhaba Dünya")
+#print("Merhaba"+" "+"Şahin")
+#print("Merhaba"+"Şahin")
+#print("Merhaba"+"Şahin")
+#print(input("Merhaba, Adın Nedir?"))
+#print("Merhaba " + input("Mehaba,Adın Nedir?") + "!")
+#print(len(input("Merhaba, Adın Nedir?")))
+#name = input("Merhaba Adın Nedir?")
+#print(name)
+#name = "Şahin"
+#print(name)
+#name = "Taşın"
+#print(name)
+#print(len(input("Merhaba, Adın Nedir?")))
+#isim = input("Merhaba, Adın Nedir?")
+#uzunluk = len(isim)
+#print(uzunluk)
+#Sayi1 = input("1nci Sayıyı Giriniz: ")
+#Sayi2 = input("2nci Sayıyı Giriniz: ")
+#Sayi3 = Sayi1
+#Sayi1 = Sayi2
+#Sayi2 = Sayi3
+#print("Sayı1 : " + Sayi1)
+#print("Sayı2 : " + Sayi2)
